9bfb0b034951bbd5a2bdccb12b4217a2505af073
Leandro Marques <leandrolmarques@gmail.com>
2017-08-02 12:45:41 -0300

    Modify pick_color function to return a struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,14 +5,12 @@ defmodule Identicon do
     |> pick_color
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-
-    [r, g, b]
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
-    hex =:crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
